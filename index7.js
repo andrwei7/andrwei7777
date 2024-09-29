f0115c1c449aad4043306e6b7f63780e0b92d3f2
andrwei7 <150726215+andrwei7@users.noreply.github.com>
2024-09-29 15:42:16 +0300

    Add files via upload

---
+++ index7.js
@@ -0,0 +1,6 @@
+alert("Salut inainte sa accesezi trebuie sa scrii , 777");
+var myName = "andrwei7";
+var yourName = prompt("Scrie aici")
+alert("my name is "+ myName + ", Te pup ti am luat ip-ul si imediat ti se inchide pc-ul " + yourName + "!");
+onClick="javascript:window.close('','_parent','');"
+    
